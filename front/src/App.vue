00d02b7a105cb44e5f00f378d9bf43a7692b4096
kwon-yeongsik <youngsik0266@gmail.com>
2022-12-01 10:39:02 +0900

    lecture : 호돌맨의 요절복통 개발쇼 - vue.js 설치 및 글 작성화면

---
+++ front/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/write">글작성</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="제목을 입력해주세요 ">
+  </div>
+  <div>
+    <textarea rows="15"></textarea>
+  </div>
+
+  <button>글 작성완료</button>
+</template>
+
+<style>
+
+</style>
